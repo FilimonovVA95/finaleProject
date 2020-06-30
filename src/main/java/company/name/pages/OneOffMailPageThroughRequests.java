0ffc600a01f012f76c1624a44710601ad72c93da
Victor <FilimonovVA.95@mail.ru>
2020-06-30 20:16:29 +0300

    Add OneOffMail class

--- src/main/java/company/name/pages/OneOffMailPageThroughRequests.java
+++ src/main/java/company/name/pages/OneOffMailPageThroughRequests.java
@@ -6,30 +6,110 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class OneOffMailPageThroughRequests{
+/**
+ * Класс для работы с временной почтой. Все методы статические. Используются GET запросы. Доступно не более 5 ящиков
+ * для одного пользователя одновременно. Время жизни одного ящика - 10 минут. Чтобы создавать еще ящики
+ * удаляйте предыдущий с помощью метода deleteEmail() перед созданием нового
+ * @Author Филимонов Виктор
+ */
+public class OneOffMailPageThroughRequests {
 
     /**
-     * Поля сайта одноразовой почты, время ожидания появления письма, время ожидания проверок наличия элементов,
-     * вебдрайвер и ссылка на тестовый стенд
+     * Используемые переменные, адрес основного URL, время ожидания письма в секундах, адрес текущего почтового ящика,
+     * ключ доступа к текущему почтовому ящику и пароль
      */
-    private static String getUrlToNewEmail = "https://post-shift.ru/api.php?action=new";
-
-
-    private static int timeWaitEmail = 60;    //сколько секунд будем ждать появление письма в ящике
-    private static int timeWait = 10;   //время ожидания при проверке
-
+    private static String stringUrl = "https://post-shift.ru/api.php?";
+    private static int timeWaitEmail = 60;
     private static String email;
     private static String keyEmail;
     private static String password;
-    private static int lastIdLetter = 0;
 
+    /**
+     * Приватный конструктор. Запрещает создание экземпляров класса т.к. все методы статичные
+     */
     private OneOffMailPageThroughRequests() {}
 
-
+    /**
+     * Возвращет строку адреса текущей почты или null если сейчас нет активной почты
+     * @return строку адреса почты
+     */
     public static String getEmail() { return email; }
 
-    public static String getNewEmail() throws IOException{
-        URL url = new URL(getUrlToNewEmail);
+    /**
+     * Создает новую временную почту и возвращет строку адреса новой почты
+     * @return строку адреса новой почты
+     * @throws IOException используется класс URL
+     */
+    public static String getNewEmail() throws IOException {
+
+        URL UrlToNewEmail = new URL(stringUrl + "action=new");
+
+        String[] stringWithEmail = getStringReturn(UrlToNewEmail).split(" ");
+
+        email = stringWithEmail[1].replace("Key:", "");
+        keyEmail = stringWithEmail[2];
+        password = null;
+
+        System.out.println(email);
+        System.out.println(keyEmail);
+
+        return email;
+    }
+
+    /**
+     * Возвращает пароль текущего ящика
+     * @return возвращает строку с паролем для текущего почтового ящика
+     * @throws IOException используется класс URL
+     */
+    public static String getPassword() throws IOException {
+        if (password != null)
+            return password;
+
+        URL UrlWaitPassword = new URL(stringUrl + "action=getlist&key=" + keyEmail);
+
+        for (int i = 0; i < timeWaitEmail; i ++) {
+            String stringWithEmail = getStringReturn(UrlWaitPassword);
+
+            if(!stringWithEmail.equals("Error: The list is empty.")) { break; }
+
+            System.out.println(stringWithEmail);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        URL UrlToPasswordByKey = new URL(stringUrl + "action=getmail&key=" + keyEmail + "&id=1&forced=1");
+
+        String mailString = getStringReturn(UrlToPasswordByKey);
+        String[] stringWithPassword = mailString.split("<p>");
+
+        password = stringWithPassword[5].replace("</p>", "").split(" ")[1];
+        return password;
+    }
+
+    /**
+     * Удаляет текущую используемую почту
+     * @throws IOException используется класс URL
+     */
+    public static void deleteEmail() throws IOException {
+        URL UrlToDeleteEmail = new URL(stringUrl + "action=delete&key=" + keyEmail);
+        HttpURLConnection connection = (HttpURLConnection) UrlToDeleteEmail.openConnection();
+        connection.setRequestMethod("GET");
+        email = null;
+        keyEmail = null;
+        password = null;
+    }
+
+    /**
+     * Принимает URL GET запроса и возвращает строку ответа
+     * @param url URL запроса для получения ответа
+     * @return строку ответа на GET запрос
+     * @throws IOException используется класс URL
+     */
+    private static String getStringReturn (URL url) throws IOException {
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
         connection.setConnectTimeout(30000);
@@ -42,22 +122,8 @@ public class OneOffMailPageThroughRequests{
 
         while ((line = bufferedReader.readLine()) != null) {
             result += line;
-            System.out.println(line);
         }
         bufferedReader.close();
-
-        System.out.println(result);
-
-        email = result;   //надо еще распарсить
-        keyEmail = result;
-
-       // System.out.println(new String(bytes, UTF_8));
-
-
-
-        return email;
-
+        return result;
     }
-
-
 }
---
+++ src/test/java/company/name/OrderTestFromClient.java
@@ -0,0 +1,192 @@
+package company.name;
+
+import company.name.pages.*;
+import io.qameta.allure.Step;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import static io.qameta.allure.Allure.step;
+
+/**
+ * Класс теста проверки функционала клиента. Проверяет возможность заказать новый тест
+ * @see company.name.pages.ClientOrderTest
+ * @Author Филимонов Виктор
+ */
+public class OrderTestFromClient {
+
+    /**
+     * Поля вебдрайвера и страницы, которую тестируем
+     */
+    private WebDriver driver = DriverManager.getDriver();
+    private ClientOrderTest clientOrderTest = new ClientOrderTest();
+
+    /**
+     * Поля переменных, необходимых при тестировании. Данные полей вводятся при создании теста
+     */
+    String nameTest = "Name";
+    String siteTest = "https://test.uxcrowd.ru/";
+    String information = "Information";
+    String segmentName = "SegmentName";
+    String task = "Task";
+
+    /**
+     * Тест, тестирует возможность заказать тестирование своего сайта
+     */
+    @Test
+    public void createTestClient() {
+
+        openTestStandStep();
+        step("Проверяем активность кнопки \"Войти\"", () -> {
+            Assert.assertTrue(clientOrderTest.  .isEnabled(), "Open test stand exception");
+        });
+
+        logInStep();
+        step("Проверяем активность кнопки \"Выйти\"", () -> {
+            Assert.assertTrue( clientOrderTest.  .isEnabled(), "Log In exception");
+        });
+
+        createTestStep();
+        step("Проверяем активность кнопки \"К выбору аудитории\"", () -> {
+            Assert.assertTrue( clientOrderTest.  .isEnabled(), "Create test exception");
+        });
+
+        inputNameTestStep(nameTest);
+        step("Проверяем правильность ввода в поле \"Название теста\"", () -> {
+            Assert.assertTrue( clientOrderTest.  .getText().equals(nameTest), "Input name test exception");
+        });
+
+        inputSiteTestStep(siteTest);
+        step("Проверяем правильность ввода в поле \"Адрес тестируемого сайта\"", () -> {
+            Assert.assertTrue( clientOrderTest.  .getText().equals(siteTest), "Input site test exception");
+        });
+
+        inputIntroductoryInformationStep(information);
+        step("Проверяем правильность ввода в поле \"Вводная информация\"", () -> {
+            Assert.assertTrue( clientOrderTest.  .getText().equals(information), "Input introductory information exception");
+        });
+
+        clickNextStep();
+        step("Проверяем активность кнопки \"К заданиям\"", () -> {
+            Assert.assertTrue( clientOrderTest.  .isEnabled(), "Passing to audience exception");
+        });
+
+        inputSegmentNameStep(segmentName);
+        step("Проверяем правильность ввода в поле \"Название сегмента\"", () -> {
+            Assert.assertTrue( clientOrderTest.  .getText().equals(segmentName), "Input segment name exception");
+        });
+
+        clickTasksStep();
+        step("Проверяем активность кнопки \"Проверка и запуск\"", () -> {
+            Assert.assertTrue( clientOrderTest.  .isEnabled(), "Passing to tasks exception");
+        });
+
+        inputTaskStep(task);
+        step("Проверяем правильность ввода в поле \"Вопросы\"", () -> {
+            Assert.assertTrue( clientOrderTest.  .getText().equals(task), "Input task exception");
+        });
+
+        clickAddTaskStep();
+        step("Проверяем активность кнопки \"Удалить\"", () -> {
+            Assert.assertTrue( clientOrderTest.  .isEnabled(), "Add task exception");
+        });
+
+        clickCheckAndRunStep();
+        step("Проверяем активность кнопки \"Запустить бесплатный тест\"", () -> {
+            Assert.assertTrue( clientOrderTest.  .isEnabled(), "Check and run exception");
+        });
+
+        clickRunFreeTestStep();
+        step("Проверяем активность кнопки \"Вернуться к списку тестов\"", () -> {
+            Assert.assertTrue( clientOrderTest.  .isEnabled(), "Run free test exception");
+        });
+
+        clickGoToListTestsStep();
+        step("Проверяем активность кнопки \"Копировать\"", () -> {
+            Assert.assertTrue( clientOrderTest.  .isEnabled(), "Test list exception");
+        });
+
+        logoutStep();
+        step("Проверяем активность кнопки \"Вход\"", () -> {
+            Assert.assertTrue( clientOrderTest.  .isEnabled(), "Logout exception");
+        });
+    }
+
+
+    @Step("Открыть тестовый стенд")
+    private void openTestStandStep() {
+        clientOrderTest.openTestStand();
+    }
+
+
+    @Step("Войти в профиль клиента")
+    private void logInStep() {
+        clientOrderTest.logIn();
+    }
+
+    @Step("Нажать кнопку \"Новый тест\"")
+    private void createTestStep() {
+        clientOrderTest.createTest();
+    }
+
+    @Step("Ввести название теста")
+    private void inputNameTestStep(String nameTest) {
+        clientOrderTest.inputNameTest(nameTest);
+    }
+
+    @Step("Ввести адрес тестируемого сайта")
+    private void inputSiteTestStep(String siteTest) {
+        clientOrderTest.inputSiteTest(siteTest);
+    }
+
+    @Step("Ввести вводную информацию")
+    private void inputIntroductoryInformationStep(String information) {
+        clientOrderTest.inputIntroductoryInformation(information);
+    }
+
+    @Step("Нажать кнопку \"К выбору аудитории\"")
+    private void clickNextStep() {
+        clientOrderTest.clickNext();
+    }
+
+    @Step("Ввести название сегмента")
+    private void inputSegmentNameStep(String segmentName) {
+        clientOrderTest.inputSegmentName(segmentName);
+    }
+
+    @Step("Нажать кнопку \"К заданиям\"")
+    private void clickTasksStep() {
+        clientOrderTest.clickTasks();
+    }
+
+    @Step("Ввести вопросы")
+    private void inputTaskStep(String task) {
+        clientOrderTest.inputTask(task);
+    }
+
+    @Step("Нажать кнопку \"Добавить\"")
+    private void clickAddTaskStep() {
+        clientOrderTest.clickAddTask();
+    }
+
+    @Step("Нажать кнопку \"Проверка и запуск\"")
+    private void clickCheckAndRunStep() {
+        clientOrderTest.clickCheckAndRun();
+    }
+
+    @Step("Нажать кнопку \"Запустить бесплатный тест\"")
+    private void clickRunFreeTestStep() {
+        clientOrderTest.clickRunFreeTest();
+    }
+
+    @Step("Нажать кнопку \"Вернуться к списку тестов\"")
+    private void clickGoToListTestsStep() {
+        clientOrderTest.clickGoToListTests();
+    }
+
+    @Step("Нажать кнопку \"Выход\"")
+    private void logoutStep() {
+        clientOrderTest.logout();
+    }
+
+}
